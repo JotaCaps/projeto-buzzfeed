144d60bc4c9389bfea9107279b61744fde04c346
JotaCaps <149966521+JotaCaps@users.noreply.github.com>
2023-12-30 22:17:35 -0300

    Finalizei a parte lógica

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -39,16 +39,35 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value:string) {
     this.answers.push(value) 
+    this.nextStep()
   }
 
-  async nextStep(){
+    async nextStep(){
     this.questionIndex += 1
 
     if(this.questionMaxIndex > this.questionIndex){
         this.questionSelected = this.questions[this.questionIndex]
     } else {
-        this.finished = true
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+        this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
   }
+
+  async checkResult(answers:string[]) {
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      } else{
+        return current
+      }
+    })
+
+    return result
+  }
+
 
 }
